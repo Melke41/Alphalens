8d559a4f760f85a6d8dd49e4b807a375c3601639
Melkamu Abyot Mekonen <melkamuabyot7@gmail.com>
2026-05-17 19:40:52 +0300

    Add Market Intelligence route and import in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Layout from './components/Layout'
 import Dashboard from './pages/Dashboard'
 import Research from './pages/Research'
 import Markets from './pages/Markets'
+import MarketIntelligence from './pages/MarketIntelligence'
 import QuantTools from './pages/QuantTools'
 import MacroFed from './pages/MacroFed'
 import AfricanMarkets from './pages/AfricanMarkets'
@@ -17,6 +18,7 @@ export default function App() {
         <Route index element={<Dashboard />} />
         <Route path="research" element={<Research />} />
         <Route path="markets" element={<Markets />} />
+        <Route path="/market-intelligence" element={<MarketIntelligence />} />
         <Route path="quant" element={<QuantTools />} />
         <Route path="macro" element={<MacroFed />} />
         <Route path="african" element={<AfricanMarkets />} />
